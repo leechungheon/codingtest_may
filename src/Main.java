3696ff7fb87b49f92ea91e6670fe82725b68bb5f
dawn_lch <chung4111@naver.com>
2024-05-23 15:03:48 +0900

    finish

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        int a=scan.nextInt();
-        int b=scan.nextInt();
+        String s=scan.next();
         Solution sol=new Solution();
-        int[]answer=sol.solution(a,b);
-        for(int i=0; i<answer.length; i++){
-            System.out.println(answer[i]+",");
-        }
+        System.out.println("답 : "+sol.solution(s));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,23 @@
 class Solution {
-    public int[] solution(int n, int s) {//2,9
-        int[] answer = {};
-        if(n>s){
-            answer=new int[1];
-            answer[0]=-1;
-            return answer;
+    public int solution(String s) {
+        int answer = 0;
+        String[] array_string=s.split(" ");
+        System.out.println("array.string.length : "+array_string.length);
+        for(int i=0; i<array_string.length; i++) {
+            System.out.print(array_string[i]+" ");
         }
-        int rest=s%n;
-        int time=s/n;
-        answer=new int[n];
-        for(int i=0; i<n; i++){
-            answer[i]=time;
+        int[] array_int=new int[array_string.length];
+        for(int i=0; i<array_string.length; i++){
+            if(!array_string[i].equals("Z")){
+                array_int[i]=Integer.parseInt(array_string[i]);
+            }else{
+                if(i>0)array_int[i]=-1*array_int[i-1];
+            }
         }
-        for(int j=0; j<rest; j++){
-            answer[n-1-j]++;
+        for(int i=0; i<array_int.length; i++){
+            answer+=array_int[i];
         }
+
         return answer;
     }
 }
