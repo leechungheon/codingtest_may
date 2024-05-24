7a1489ae179761f697c6fabff49b0f15fadc67fd
dawn_lch <chung4111@naver.com>
2024-05-25 00:17:43 +0900

    finish

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,29 @@
 class Solution {
-    
     public String solution(String letter) {
-        String answer = "";
+        String answer ="";
+        StringBuilder sb=new StringBuilder();
+        String[] s={
+                ".-", "-...", "-.-.", "-..",
+                ".", "..-.", "--.", "....",
+                "..", ".---", "-.-", ".-..",
+                "--", "-.", "---", ".--.",
+                "--.-", ".-.", "...", "-",
+                "..-", "...-", ".--", "-..-",
+                "-.--", "--.."};
+        String[] alpha = {"a", "b", "c", "d", "e", "f", "g",
+                "h", "i", "j", "k", "l", "m", "n",
+                "o", "p", "q", "r", "s", "t", "u",
+                "v", "w", "x", "y", "z"};
+
+        String[] let=letter.split(" ");
+        for(int i=0; i<let.length; i++){
+            for(int j=0; j<s.length; j++){
+                if(let[i].equals(s[j])){
+                    sb.append(alpha[j]);
+                }
+            }
+        }
+        answer=sb.toString();
         return answer;
     }
 }
