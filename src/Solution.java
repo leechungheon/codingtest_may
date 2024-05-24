4fe2d53f735e12660394b33b1139330b33c2a18a
dawn_lch <chung4111@naver.com>
2024-05-25 00:50:04 +0900

    finish

--- src/Solution.java
+++ src/Solution.java
@@ -1,29 +1,23 @@
 class Solution {
-    public String solution(String letter) {
-        String answer ="";
-        StringBuilder sb=new StringBuilder();
-        String[] s={
-                ".-", "-...", "-.-.", "-..",
-                ".", "..-.", "--.", "....",
-                "..", ".---", "-.-", ".-..",
-                "--", "-.", "---", ".--.",
-                "--.-", ".-.", "...", "-",
-                "..-", "...-", ".--", "-..-",
-                "-.--", "--.."};
-        String[] alpha = {"a", "b", "c", "d", "e", "f", "g",
-                "h", "i", "j", "k", "l", "m", "n",
-                "o", "p", "q", "r", "s", "t", "u",
-                "v", "w", "x", "y", "z"};
-
-        String[] let=letter.split(" ");
-        for(int i=0; i<let.length; i++){
-            for(int j=0; j<s.length; j++){
-                if(let[i].equals(s[j])){
-                    sb.append(alpha[j]);
-                }
+    public int solution(int[] sides) {
+        int answer = 0;
+        //주어진 변이 가장 큰 변일 때
+        if(sides[0]>=sides[1]){
+            for(int i=1; i<sides[0]; i++){
+                if(i+sides[1]>sides[0])answer++;
+            }
+        }else{
+            for(int i=1; i<sides[1]; i++){
+                if(i+sides[0]>sides[1])answer++;
             }
         }
-        answer=sb.toString();
+        //추가된 변이 가장 큰 변일 때
+        int large=0;
+        if(sides[0]>=sides[1])large=sides[0];
+        else {large=sides[1];}
+        for(int i=large; i<sides[0]+sides[1]; i++){
+            answer++;
+        }
         return answer;
     }
 }
