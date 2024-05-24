2fc8d5961024ab0b0fca538bc6d69877b6072bef
dawn_lch <chung4111@naver.com>
2024-05-24 23:10:11 +0900

    finish

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        String s=scan.next();
         Solution sol=new Solution();
-        System.out.println("답 : "+sol.solution(s));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,7 @@
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
-        String[] array_string=s.split(" ");
-        System.out.println("array.string.length : "+array_string.length);
-        for(int i=0; i<array_string.length; i++) {
-            System.out.print(array_string[i]+" ");
-        }
-        int[] array_int=new int[array_string.length];
-        for(int i=0; i<array_string.length; i++){
-            if(!array_string[i].equals("Z")){
-                array_int[i]=Integer.parseInt(array_string[i]);
-            }else{
-                if(i>0)array_int[i]=-1*array_int[i-1];
-            }
-        }
-        for(int i=0; i<array_int.length; i++){
-            answer+=array_int[i];
-        }
-
+    
+    public String solution(String letter) {
+        String answer = "";
         return answer;
     }
 }
