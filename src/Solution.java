3013612470bb5988f3a800c9960cb5d6c13e9364
dawn_lch <chung4111@naver.com>
2024-05-26 15:07:03 +0900

    first

--- src/Solution.java
+++ src/Solution.java
@@ -1,42 +1,35 @@
 class Solution {
     public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
         long answer = -1;
-        //시간순대로 오름차순 정렬 후, 운반 완료되는 w[t] 시점에서 시간 구하기-카운팅정렬 사용
-        int max=0;
-        for(int i=0; i<t.length; i++){
-            if(max<t[i])max=t[i];
-        }
-        int[] count=new int[max+1];
-        int[] resultT = new int[t.length];
-        int[] resultG = new int[g.length];
-        int[] resultS = new int[s.length];
-        int[] resultW = new int[w.length];
-        int[] result=new int[t.length];
-        // 각 원소의 개수를 카운팅 배열에 저장
-        for (int i = 0; i < t.length; i++) {
-            count[t[i]]++;
-        }
-        // 카운팅 배열의 누적 합 계산
-        for (int i = 1; i <= max; i++) {
-            count[i] += count[i - 1];
-        }
-        // **결과 배열에 원소 저장**
-        for (int i = t.length - 1; i >= 0; i--) {//끝인덱스부터 0번째 인덱스
-            int index = count[t[i]] - 1;
-            resultT[index] = t[i];
-            resultG[index] = g[i];
-            resultS[index] = s[i];
-            resultW[index] = w[i];
-            count[t[i]]--;
-        }
-        // 정렬된 결과를 원래 배열 t에 복사
-        for (int i = 0; i < t.length; i++) {
-            t[i] = resultT[i];
-            g[i] = resultG[i];
-            s[i] = resultS[i];
-            w[i] = resultW[i];
-        }
+        long left=1;
+        long right=(long)(10e9*2*10e5*2);
+        int len=s.length;
 
+        while(left<=right){
+            long mid=(left+right)/2; //운반 시간 지정
+
+            //도시 순회
+            int gold=0, silver=0, sum=0;
+            for(int i=0; i<len; i++){
+                int weight=w[i];
+                int time=t[i];
+
+                long cnt=mid/(time*2);
+                //몇 번 왕복운반 가능한 지
+                if((mid%(time*2))>=time){
+                    cnt++;
+                }
+                gold+=Math.min(g[i], weight*cnt);//min함수내 더 작은 것 대입
+                silver+=Math.min(s[i], weight*cnt);
+                sum+=Math.min(g[i]+s[i], weight*cnt);
+            }
+            if(gold>=a&&silver>=b && sum>=a+b){
+                answer=mid;
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }
 
         return answer;
     }
