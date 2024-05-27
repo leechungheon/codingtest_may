de1535aeb29794c614a54cdc0d45fe6a66fbb93c
dawn_lch <chung4111@naver.com>
2024-05-27 20:05:07 +0900

    first

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         Solution sol=new Solution();
-        int g[]={70,70,0};
-        int s[]={0,0,500};
-        int w[]={100,100,2};
-        int t[]={4,8,1};
-        System.out.println(sol.solution(90,500,g,s,w,t));
+        int[][] wires = {
+                {1, 3},
+                {2, 3},
+                {3, 4},
+                {4, 5},
+                {4, 6},
+                {4, 7},
+                {7, 8},
+                {7, 9}
+        };
+        System.out.println(sol.solution(9,wires));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,39 @@
 class Solution {
-    public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
-        long answer = -1;
-        long left=1;
-        long right=(long)(10e9*2*10e5*2);
-        int len=s.length;
+    int N, min;          // 전체 노드 수(N)와 최소 송전탑 개수 차이(min)를 저장하는 변수
+    int[][] map;         // 전력망 연결 정보를 저장하는 이차원 배열
+    int[] vst;           // 방문 여부를 저장하는 배열
 
-        while(left<=right){
-            long mid=(left+right)/2; //운반 시간 지정
+    // DFS를 통해 전력망을 탐색하고, 각 전력망의 송전탑 개수를 계산하며 최소 송전탑 개수 차이를 업데이트하는 함수
+    int dfs(int n) {
+        vst[n] = 1;      // 현재 노드를 방문했음을 표시
+        int child = 1;   // 현재 노드를 포함한 송전탑 개수를 저장하는 변수
 
-            //도시 순회
-            int gold=0, silver=0, sum=0;
-            for(int i=0; i<len; i++){
-                int weight=w[i];
-                int time=t[i];
-
-                long cnt=mid/(time*2);
-                //몇 번 왕복운반 가능한 지
-                if((mid%(time*2))>=time){
-                    cnt++;
-                }
-                gold+=Math.min(g[i], weight*cnt);//min함수내 더 작은 것 대입
-                silver+=Math.min(s[i], weight*cnt);
-                sum+=Math.min(g[i]+s[i], weight*cnt);
-            }
-            if(gold>=a&&silver>=b && sum>=a+b){
-                answer=mid;
-                right=mid-1;
-            }else{
-                left=mid+1;
+        // 현재 노드와 연결된 모든 노드에 대해 탐색
+        for (int i = 1; i <= N; i++) {
+            if (vst[i] == 0 && map[n][i] == 1) { //
+                vst[i] = 1;                       // 해당 노드를 방문했음을 표시
+                child += dfs(i);                  // 해당 노드에서 재귀적으로 탐색하고, 송전탑 개수를 누적
             }
         }
+        // 해당 서브트리의 송전탑 개수와 나머지 전력망의 송전탑 개수 차이를 계산하여 최소값 갱신
+        min = Math.min(min, Math.abs(child - (N - child)));
+        return child;  // 현재 서브트리의 송전탑 개수 반환
+    }
 
-        return answer;
+    // 전력망을 구성하고 최소 송전탑 개수 차이를 계산하는 함수
+    public int solution(int n, int[][] wires) {
+        N = n;                             // 전체 노드 수 저장
+        min = n;                           // 최소 송전탑 개수 차이 초기화
+        map = new int[n+1][n+1];          // 전력망 연결 정보를 저장하는 이차원 배열 초기화
+        vst = new int[n+1];               // 방문 여부를 저장하는 배열 초기화
+
+        // 전력망 연결 정보 입력
+        for (int[] wire : wires) {
+            int a = wire[0], b = wire[1];
+            map[a][b] = map[b][a] = 1;   // 두 노드가 연결되어 있음을 표시
+        }
+
+        dfs(1);  // DFS 탐색 시작 (시작 노드는 1로 가정)
+        return min;  // 최소 송전탑 개수 차이 반환
     }
 }
