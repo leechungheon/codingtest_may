83484ec02ab0d6a41735f74379dd827465aa308e
dawn_lch <chung4111@naver.com>
2024-05-30 01:43:34 +0900

    first

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-import java.awt.event.*;
+//JFrame을 새로이 구현한다.
 class MyFrame extends JFrame {
     private JButton button;
     private JLabel label;
@@ -7,10 +7,12 @@ class MyFrame extends JFrame {
         this.setSize(300, 200);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("이벤트 예제");
-        JPanel panel = new JPanel();
         button = new JButton("버튼을 누르시오");
         label = new JLabel("아직 버튼이 눌려지지 않았습니다");
         button.addActionListener(new MyListener());
+
+
+        JPanel panel = new JPanel();
         panel.add(button);
         panel.add(label);
         this.add(panel);
--- src/MyListener.java
+++ src/MyListener.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
 import java.awt.event.*;
+//리스너를 상속받는다. 동작에 반응해 이벤트를 처리한다.
 class MyListener implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         JButton button = (JButton) e.getSource();
         button.setText("마침내 버튼이 눌려졌습니다.");
-// label.setText("마침내 버튼이 눌려졌습니다.");
+        // label.setText("마침내 버튼이 눌려졌습니다.");
     }
 }
