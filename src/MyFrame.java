36d120703a3c8bdae73e47c22951343aed28639f
dawn_lch <chung4111@naver.com>
2024-06-05 15:03:30 +0900

    first

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,35 +1,12 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class MyFrame extends JFrame {
-    private JButton button1;
-    private JButton button2;
-    private JPanel panel;
-
     public MyFrame() {
-        this.setSize(300, 200);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("이벤트 예제");
-        panel = new JPanel();
-        button1 = new JButton("노란색");
-        button1.addActionListener(new MyListener());
-        panel.add(button1);
-        button2 = new JButton("핑크색");
-        button2.addActionListener(new MyListener());
-        panel.add(button2);
-        this.add(panel);
-        this.setVisible(true);
+        add(new MyPanel());
+        setSize(300, 500);
+        setVisible(true);
     }
-        private class MyListener implements ActionListener {
-            public void actionPerformed(ActionEvent e){
-                if(e.getSource()==button1){
-                    panel.setBackground(Color.YELLOW);
-                }else if(e.getSource()==button2){
-                    panel.setBackground(Color.PINK);
-                }
-            }
-        }
-
+    public static void main(String[] arg) {
+        new MyFrame();
     }
+}
